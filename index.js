3121073f0697b70071dd2af14d523a7f1b3687fb
aharri64 <haveacompleteday@gmail.com>
2021-02-09 10:48:34 -0700

    all set

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(require('morgan')('dev'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.send
+})
+
+
+
+
+const server = app.listen(port, function () {
+    console.log('...listening on', port);
+});
